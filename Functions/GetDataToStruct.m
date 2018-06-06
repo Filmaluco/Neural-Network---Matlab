5e557478b677966174891a3a696c02c3629aece3
FilipeA <filipera1995@hotmail.com>
2018-06-06 18:24:14 +0100

    Added: GetDataToStruct

--- Functions/GetDataToStruct.m
+++ Functions/GetDataToStruct.m
@@ -1,20 +1,24 @@
-function excelData = GetDataToStruct( imagens , fileName)
+function imagens = GetDataToStruct( imagens , fileName)
 
 
     %TODO get as param
-    
     fileName = '..\Tema 1 - RN\ClassificaçãoFolhas.xlsx';
     
    
-    excelData = xlsread(fileName, 'B:B')
-
-    a = size(imagens);
-    n = a(2)
+    [num,txt,raw] = xlsread(fileName);
     
-
+    a = size(imagens);
+    b = size(raw);
+    n = a(2);
+    m = b(1);
+    
     for k=1 : n
-       
-        
+        %first line is the names of the columms
+       for i=2 : m
+          if raw{i,1} == imagens(k).id 
+              imagens(k).especie = raw{i,2};
+              imagens(k).subEspecie = raw{i,3};
+          end
         
     end
     
