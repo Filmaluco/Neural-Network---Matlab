91fac1261528937ccdd60659a5a5429bfe0948f6
BenVictory <38571894+BenVictory@users.noreply.github.com>
2018-07-03 17:41:45 +0100

    added: scale

--- Functions/ConverterImagemParaMatrizBinaria.m
+++ Functions/ConverterImagemParaMatrizBinaria.m
@@ -1,14 +1,19 @@
-function A=ConverterImagemParaMatrizBinaria(PATH)
-A=imread(PATH);
+function matriz=ConverterImagemParaMatrizBinaria(PATH,scale)
+matriz=imread(PATH);
+%rotate
+%resize
 
-ta = size(A);
+matriz = imresize(matriz,[scale,scale]);
+
+
+ta = size(matriz);
 
 for i=1:ta(1,1) 
     for j=1:ta(1,2)    
-        if(A(i,j)<125)     
-            A(i,j)=0;  %0 -> PRETOer
+        if(matriz(i,j)<125)     
+            matriz(i,j)=0;  %0 -> PRETOer
         else
-            A(i,j)=1;   % 1 -> BRANCO
+            matriz(i,j)=1;   % 1 -> BRANCO
         end
     end
 end
