a6fa21b5e98547fc8d323850c0e452805dc44e12
FilipeA <filipera1995@hotmail.com>
2018-07-04 17:06:06 +0100

    Added: ImageResizing :poop:

--- Functions/ConverterImagemParaMatrizBinaria.m
+++ Functions/ConverterImagemParaMatrizBinaria.m
@@ -1,37 +1,7 @@
 function matriz=ConverterImagemParaMatrizBinaria(PATH,scale)
 matriz=imread(PATH);
-%rotate
-%resize
-blankimage = zeros(scale);
-
-
-%matriz = imresize(matriz,[scale,scale]);
-imagem = ImageRotation(PATH);
-ta = size(imagem); 
-%imshowpair(imagem,matriz,'montage');
- 
-while ta(1,2) > scale
-    ta(1,1)= ta(1,1)/2;
-    ta(1,2)=ta(1,2)/2;
-    imagem = imresize(imagem,[ta(1,1),ta(1,2)]);
-end
-for x=1 : ta(1,1)
-    for y =1 : ta(1,2)
-       blankimage(x,y) = imagem(x,y);
-    end
-end
-image(blankimage);
-disp(blankimage);
-pause(2);
-
-  
-    
-
-    
-
-pause(2);
-ta = size(matriz);
-display(ta);
+%imagem = ImageRotation(PATH);
+ta = size(matriz); 
 
 for i=1:ta(1,1) 
     for j=1:ta(1,2)    
@@ -42,6 +12,11 @@ for i=1:ta(1,1)
         end
     end
 end
+
+matriz = convertImageToData(matriz, scale);
+
+imshow(matriz);
+pause(2);
 
 
 end
---
+++ Functions/convertImageToData.m
@@ -0,0 +1,47 @@
+function ImagemDataSet=convertImageToData(originalImage, scale) 
+
+%Control Variables
+    padding = 10; %padding to garanty that the border is never crossed
+
+tam = size(originalImage);
+originalX = tam(1,1);
+originalY = tam(1,2);
+
+%even do its rotated the image can have both scenarios
+% x > y and x < y 
+%this is because do rotation only applies to if it pass's a "limit"
+if originalX > originalY
+    ImagemDataSet = zeros(originalX + padding);
+else
+    ImagemDataSet = zeros(originalY + padding);
+end
+
+%Get the center of the "moldure"
+tam = size(ImagemDataSet);
+DataSetCenterX = floor(tam(1,1)/2);
+DataSetCenterY = floor(tam(1,2)/2); %...%
+
+%Copy the original image to the center of the "moldure"
+
+im_x = 0;
+
+inicialX = DataSetCenterX - floor(originalX/2);
+inicialY = DataSetCenterY - floor(originalY/2);
+
+
+endX = DataSetCenterX + floor(originalX/2)-1;
+endY = DataSetCenterY + floor(originalY/2)-1;
+
+
+for x = inicialX: endX
+    im_x = im_x + 1;
+    im_y = 0;
+    for y = inicialY: endY
+        im_y = im_y + 1;
+        ImagemDataSet(x,y) = originalImage(im_x, im_y);
+    end
+end
+
+ImagemDataSet = imresize(ImagemDataSet, [scale scale]);
+
+end
