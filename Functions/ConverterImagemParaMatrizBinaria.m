60a8477548c613989f7a33a81fa185dd60ec8099
BenVictory <38571894+BenVictory@users.noreply.github.com>
2018-07-04 16:50:30 +0100

    Added : rotateThisImage :100:

--- Functions/ConverterImagemParaMatrizBinaria.m
+++ Functions/ConverterImagemParaMatrizBinaria.m
@@ -4,25 +4,27 @@ matriz=imread(PATH);
 %resize
 blankimage = zeros(scale);
 
-
+matriz = rotateThisImage(matriz);
+imshow(matriz);
+pause(2);
 %matriz = imresize(matriz,[scale,scale]);
-imagem = ImageRotation(PATH);
-ta = size(imagem); 
+%imagem = ImageRotation(PATH);
+%ta = size(imagem); 
 %imshowpair(imagem,matriz,'montage');
  
-while ta(1,2) > scale
-    ta(1,1)= ta(1,1)/2;
-    ta(1,2)=ta(1,2)/2;
-    imagem = imresize(imagem,[ta(1,1),ta(1,2)]);
-end
-for x=1 : ta(1,1)
-    for y =1 : ta(1,2)
-       blankimage(x,y) = imagem(x,y);
-    end
-end
-image(blankimage);
-disp(blankimage);
-pause(2);
+% while ta(1,2) > scale
+%     ta(1,1)= ta(1,1)/2;
+%     ta(1,2)=ta(1,2)/2;
+%     imagem = imresize(imagem,[ta(1,1),ta(1,2)]);
+% end
+% for x=1 : ta(1,1)
+%     for y =1 : ta(1,2)
+%        blankimage(x,y) = imagem(x,y);
+%     end
+% end
+% image(blankimage);
+% disp(blankimage);
+% pause(2);
 
   
     
